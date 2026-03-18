a4de82a616a6a8c4eaa7cca18bc2661d8b622296
Hubert Stach <hubertstach@student.agh.edu.pl>
2026-03-18 18:46:01 +0100

    added .sh and .bat files for building

--- build.bat
+++ build.bat
@@ -1,31 +1,28 @@
 @echo off
 setlocal
 
-:: Zmienne konfiguracyjne
 set BUILD_DIR=build
 set BUILD_TYPE=Debug
 set PROJECT_NAME=easyFEM
 
 echo ==========================================
-echo  Budowanie projektu %PROJECT_NAME% (%BUILD_TYPE%)
+echo  Building project %PROJECT_NAME% (%BUILD_TYPE%)
 echo ==========================================
 
 :: 1. Konfiguracja (generowanie plikow budowania)
-echo [1/2] Konfiguracja CMake...
+echo [1/2] Configuring CMake...
 cmake -S . -B %BUILD_DIR% -DCMAKE_BUILD_TYPE=%BUILD_TYPE%
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 :: 2. Wlasciwe budowanie (kompilacja)
-echo [2/2] Kompilacja przez CMake...
+echo [2/2] Compilating using CMake...
 cmake --build %BUILD_DIR% --config %BUILD_TYPE% --parallel
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 echo ==========================================
-echo  Sukces! Projekt zostal zbudowany.
-echo  Aby go uruchomic, wejdz do folderu %BUILD_DIR%
-echo  i uruchom %PROJECT_NAME%.exe lub wpisz w terminalu:
+echo  Project has been built succesfully
+echo  to run %PROJECT_NAME%.exe write:
 echo  .\%BUILD_DIR%\%BUILD_TYPE%\%PROJECT_NAME%.exe
 echo ==========================================
 
-:: Zatrzymuje zamykanie okna, jesli uzytkownik kliknal plik 2 razy
 pause
--- build.sh
+++ build.sh
@@ -9,23 +9,23 @@ BUILD_TYPE="Debug"
 PROJECT_NAME="easyFEM"
 
 echo "=========================================="
-echo " Budowanie projektu $PROJECT_NAME ($BUILD_TYPE) "
+echo " Building project $PROJECT_NAME ($BUILD_TYPE) "
 echo "=========================================="
 
 # 1. Konfiguracja (generowanie plików budowania)
-echo "[1/2] Konfiguracja CMake..."
+echo "[1/2] Configuring CMake..."
 # Tworzy folder build/ i konfiguruje projekt.
 # CMake sam dobierze najlepszy domyślny system budowania dla systemu użytkownika.
 cmake -S . -B $BUILD_DIR -DCMAKE_BUILD_TYPE=$BUILD_TYPE
 
 # 2. Właściwe budowanie (kompilacja)
-echo "[2/2] Kompilacja przez CMake..."
+echo "[2/2] Compilating using CMake..."
 # Uniwersalna komenda kompilacji - ukrywa to czy pod spodem jest Make, Ninja czy coś innego.
 # --parallel sprawia, że kompilacja użyje wielu rdzeni procesora, co ją znacznie przyspieszy.
 cmake --build $BUILD_DIR --config $BUILD_TYPE --parallel
 
 echo "=========================================="
-echo " Sukces! Projekt został zbudowany."
-echo " Aby go uruchomić, wpisz w terminalu:"
-echo " ./$BUILD_DIR/$PROJECT_NAME"
+echo  "Project has been built succesfully"
+echo  "to run %PROJECT_NAME%.exe write:"
+echo  "".\%BUILD_DIR%\%BUILD_TYPE%\%PROJECT_NAME%.exe""
 echo "=========================================="
